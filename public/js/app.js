a926587f51e8181c8e5aa324a533dbe24a0a455e
Ariana Cabana <acabana.0904@gmail.com>
2017-06-20 17:59:03 -0500

    insertando materialize

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
-$(_ =>{
-  const h1 = $('<h1>Probando server y jQuery</h1>');
-
-  $('body').append(h1);
-});
+// $(_ =>{
+//   const h1 = $('<h1>Probando server y jQuery</h1>');
+//
+//   $('body').append(h1);
+// });
+// const render = (root) => {
+//   root.empty();
+//   const wrapper = $('<section id="slider"></section>');
+//   wrapper.append(Header(_ => render(root)));
+//   root.append(wrapper);
+// }
--- public/js/components/screen1.js
+++ public/js/components/screen1.js
@@ -1,14 +1,18 @@
-$(_ =>{
-  const sliderIntro = $('<div id="slider-img" class="owl-nome owl-carousel"></div>');
+$( () =>{
+  const sliderIntro = $('<div id="slider-img" class="carousel carousel-slider center" data-indicators="true"></div>');
 
-  const slider1 = $('<div id="slider1" class="item"><img src="img/icons/icon-people.png" alt="slider1"/></div>');
+  const slider1 = $('<div id="slider1" class="carousel-fixed-item center"><img src="img/icons/icon-people.png" alt="slider1"></div>');
   const texto1 = $('<h3>Paga a tus amigos</h3><span>Paga a quien quieras desde donde quieras, sin usar efectivo</span>');
-  const slider2 = $('<div id="slider2" class="item"><img src="img/icons/happy-person.png" alt="slider2"></div>');
+  const slider2 = $('<div id="slider2" class="carousel-fixed-item center"><img src="img/icons/happy-person.png" alt="slider2"></div>');
   const texto2 = $('<h3>Sin número de cuenta</h3><span>Elige a quién pagar desde tu lista de contactos</span>');
-  const slider3 = $('<div id="slider3" class="item"><img src="img/icons/group-people.png" alt="slider3"></div>');
+  const slider3 = $('<div id="slider3" class="carousel-fixed-item center"><img src="img/icons/group-people.png" alt="slider3"></div>');
   const texto3 = $('<h3>Gratis y seguro</h3><span>La transferencia es inmediata y gratuita de una cuenta a otra</span>');
 
+  const btn = $('<button class="btn">REGISTRARME</button>');
+
   $('#slider').append(sliderIntro);
+  $('#slider').append(btn);
+
   $('#slider-img').append(slider1);
   $('#slider-img').append(slider2);
   $('#slider-img').append(slider3);
@@ -16,25 +20,3 @@ $(_ =>{
   $('#slider2').append(texto2);
   $('#slider3').append(texto3);
 });
-
-$('.owl-nome').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots: true,
-    navText: [
-      "<i class='fa fa-angle-left'></i>",
-      "<i class='fa fa-angle-right'></i>"
-      ],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
