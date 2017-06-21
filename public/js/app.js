23d05b99154dbd27aa79dc1fe64b64980be8cc17
Ariana Cabana <acabana.0904@gmail.com>
2017-06-21 00:15:18 -0500

    modificando el slide

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,14 @@
 const render = (root) =>{
   root.empty();
   const wrapper = $('<div class="wrapper"></div>');
-  wrapper.append(Screen1());
-  // if (state.screen1 == null) {
-  //   console.log("hola screen1");
-  //   wrapper.append(Screen1(_ => render(root)));
-  // } else{
-  //   console.log("hola screen2");
-  //   // wrapper.append(Screen2(_ => render(root)));
-  // }
+  // wrapper.append(Screen1());
+  if (state.screen1 == null) {
+    console.log("hola screen1");
+    wrapper.append(Screen1(_ => render(root)));
+  } else{
+    console.log("hola screen2");
+    // wrapper.append(Screen2(_ => render(root)));
+  }
   root.append(wrapper);
 }
 
--- public/js/components/screen1.js
+++ public/js/components/screen1.js
@@ -1,26 +1,23 @@
 'use strict';
 const Screen1 = (update) =>{
-  const sliderIntro = $('<div id="slider-img" class="carousel carousel-slider" data-indicators="true"></div>');
-  const slider1 = $('<div id="slider1" class="carousel-item"><img src="img/icons/icon-people.png" alt="slider1"></div>');
-  const texto1 = $('<h3>Paga a tus amigos</h3><span>Paga a quien quieras desde donde quieras, sin usar efectivo</span>');
-  const slider2 = $('<div id="slider2" class="carousel-item"><img src="img/icons/happy-person.png" alt="slider2"></div>');
-  const texto2 = $('<h3>Sin número de cuenta</h3><span>Elige a quién pagar desde tu lista de contactos</span>');
-  const slider3 = $('<div id="slider3" class="carousel-item"><img src="img/icons/group-people.png" alt="slider3"></div>');
-  const texto3 = $('<h3>Gratis y seguro</h3><span>La transferencia es inmediata y gratuita de una cuenta a otra</span>');
-
-  const btn = $('<button class="btn">REGISTRARME</button>');
+  const sliderIntro = $('<div id="sliderIntro" class="carousel carousel-slider center" data-indicators="true"></div>');
+  const slider1 = $('<div id="slider1" class="carousel-item center"><img src="img/icons/icon-people.png" alt="slider1"></div>');
+  const texto1 = $('<span class="black-text">Paga a tus amigos</span><p>Paga a quien quieras desde donde quieras, sin usar efectivo</p>');
+  const slider2 = $('<div id="slider2" class="col s12 carousel-item center"><img src="img/icons/happy-person.png" alt="slider2"></div>');
+  const texto2 = $('<span>Sin número de cuenta</h3><span>Elige a quién pagar desde tu lista de contactos</p>');
+  const slider3 = $('<div id="slider3" class="col s12 carousel-item center"><img src="img/icons/group-people.png" alt="slider3"></div>');
+  const texto3 = $('<span>Gratis y seguro</span><p>La transferencia es inmediata y gratuita de una cuenta a otra</p>');
+  const btn = $('<div class="carousel-fixed-item center"><button class="btn waves-effect waves-light" type="submit" name="action">REGISTRARME</i></button></div>');
 
   $('#root').append(sliderIntro);
   $('#root').append(btn);
-
-  sliderIntro.append(slider1);
-  sliderIntro.append(slider2);
-  sliderIntro.append(slider3);
-
   slider1.append(texto1);
   slider2.append(texto2);
   slider3.append(texto3);
 
+  sliderIntro.append(slider1);
+  sliderIntro.append(slider2);
+  sliderIntro.append(slider3);
 
   btn.on('click',(e)=>{
     e.preventDefault();
