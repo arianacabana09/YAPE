0d5d8859e6f2952bae7a98201b41c8e3895c9082
Ariana Cabana <acabana.0904@gmail.com>
2017-06-21 02:21:15 -0500

    screen 1 por finalizar

--- public/js/components/screen1.js
+++ public/js/components/screen1.js
@@ -1,16 +1,15 @@
 'use strict';
 const Screen1 = (update) =>{
-  const sliderIntro = $('<div id="sliderIntro" class="carousel carousel-slider center" data-indicators="true"></div>');
+  const sliderIntro = $('<div id="sliderIntro" class="carousel carousel-slider center col-s12" data-indicators="true"></div>');
   const slider1 = $('<div id="slider1" class="carousel-item center"><img src="img/icons/icon-people.png" alt="slider1"></div>');
-  const texto1 = $('<span class="black-text">Paga a tus amigos</span><p>Paga a quien quieras desde donde quieras, sin usar efectivo</p>');
+  const texto1 = $('<h5><strong>Paga a tus amigos</strong></h5><p>Paga a quien quieras desde donde quieras, sin usar efectivo</p>');
   const slider2 = $('<div id="slider2" class="col s12 carousel-item center"><img src="img/icons/happy-person.png" alt="slider2"></div>');
-  const texto2 = $('<span>Sin número de cuenta</h3><span>Elige a quién pagar desde tu lista de contactos</p>');
+  const texto2 = $('<h5><strong>Sin número de cuenta</strong></h5><p>Elige a quién pagar desde tu lista de contactos</p>');
   const slider3 = $('<div id="slider3" class="col s12 carousel-item center"><img src="img/icons/group-people.png" alt="slider3"></div>');
-  const texto3 = $('<span>Gratis y seguro</span><p>La transferencia es inmediata y gratuita de una cuenta a otra</p>');
-  const btn = $('<div class="carousel-fixed-item center"><button class="btn waves-effect waves-light" type="submit" name="action">REGISTRARME</i></button></div>');
+  const texto3 = $('<h5><strong>Gratis y seguro</strong></h5><p>La transferencia es inmediata y gratuita de una cuenta a otra</p>');
+  const btn = $('<div id="btn" class="center col s12"><button class=" btn waves-effect waves-light" type="submit" name="action">REGISTRARME</button></div>');
 
   $('#root').append(sliderIntro);
-  $('#root').append(btn);
   slider1.append(texto1);
   slider2.append(texto2);
   slider3.append(texto3);
@@ -19,9 +18,10 @@ const Screen1 = (update) =>{
   sliderIntro.append(slider2);
   sliderIntro.append(slider3);
 
+  $('#root').append(btn);
   btn.on('click',(e)=>{
     e.preventDefault();
-    state.screen1 = null;
+    state.screen1 = false;
     state.screen2 = true;
     update();
   });
