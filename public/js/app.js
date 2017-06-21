b50624a065db52aa97541e82c44107422ec8d69a
Ariana Cabana <acabana.0904@gmail.com>
2017-06-20 22:18:44 -0500

    carousel con materialize

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,29 @@
-// $(_ =>{
-//   const h1 = $('<h1>Probando server y jQuery</h1>');
-//
-//   $('body').append(h1);
-// });
-// const render = (root) => {
-//   root.empty();
-//   const wrapper = $('<section id="slider"></section>');
-//   wrapper.append(Header(_ => render(root)));
-//   root.append(wrapper);
-// }
+'use strict';
+
+const render = (root) =>{
+  root.empty();
+  const wrapper = $('<div class="wrapper"></div>');
+  wrapper.append(Screen1());
+  // if (state.screen1 == null) {
+  //   console.log("hola screen1");
+  //   wrapper.append(Screen1(_ => render(root)));
+  // } else{
+  //   console.log("hola screen2");
+  //   // wrapper.append(Screen2(_ => render(root)));
+  // }
+  root.append(wrapper);
+}
+
+
+const state = {
+  screen2 : null
+  // registro : null,
+  // telefono :null,
+  // code: null
+};
+
+$(_ =>{
+  const root = $('#root');
+  render(root);
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+});
--- public/js/components/screen1.js
+++ public/js/components/screen1.js
@@ -1,22 +1,33 @@
-$( () =>{
-  const sliderIntro = $('<div id="slider-img" class="carousel carousel-slider center" data-indicators="true"></div>');
-
-  const slider1 = $('<div id="slider1" class="carousel-fixed-item center"><img src="img/icons/icon-people.png" alt="slider1"></div>');
+'use strict';
+const Screen1 = (update) =>{
+  const sliderIntro = $('<div id="slider-img" class="carousel carousel-slider" data-indicators="true"></div>');
+  const slider1 = $('<div id="slider1" class="carousel-item"><img src="img/icons/icon-people.png" alt="slider1"></div>');
   const texto1 = $('<h3>Paga a tus amigos</h3><span>Paga a quien quieras desde donde quieras, sin usar efectivo</span>');
-  const slider2 = $('<div id="slider2" class="carousel-fixed-item center"><img src="img/icons/happy-person.png" alt="slider2"></div>');
+  const slider2 = $('<div id="slider2" class="carousel-item"><img src="img/icons/happy-person.png" alt="slider2"></div>');
   const texto2 = $('<h3>Sin número de cuenta</h3><span>Elige a quién pagar desde tu lista de contactos</span>');
-  const slider3 = $('<div id="slider3" class="carousel-fixed-item center"><img src="img/icons/group-people.png" alt="slider3"></div>');
+  const slider3 = $('<div id="slider3" class="carousel-item"><img src="img/icons/group-people.png" alt="slider3"></div>');
   const texto3 = $('<h3>Gratis y seguro</h3><span>La transferencia es inmediata y gratuita de una cuenta a otra</span>');
 
   const btn = $('<button class="btn">REGISTRARME</button>');
 
-  $('#slider').append(sliderIntro);
-  $('#slider').append(btn);
+  $('#root').append(sliderIntro);
+  $('#root').append(btn);
 
-  $('#slider-img').append(slider1);
-  $('#slider-img').append(slider2);
-  $('#slider-img').append(slider3);
-  $('#slider1').append(texto1);
-  $('#slider2').append(texto2);
-  $('#slider3').append(texto3);
-});
+  sliderIntro.append(slider1);
+  sliderIntro.append(slider2);
+  sliderIntro.append(slider3);
+
+  slider1.append(texto1);
+  slider2.append(texto2);
+  slider3.append(texto3);
+
+
+  btn.on('click',(e)=>{
+    e.preventDefault();
+    state.screen1 = null;
+    state.screen2 = true;
+    update();
+  });
+
+  return sliderIntro;
+}
